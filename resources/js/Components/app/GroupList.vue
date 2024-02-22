0ab60481e7b2e568e209a88668c509815608675c
psalmistcom <dadewale9@gmail.com>
2024-02-22 13:37:15 +0100

    fixes

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -11,7 +11,7 @@ const searchKeyword = ref('')
         <h2 class="text-xl font-bold mb-4">My Groups</h2>
         <TextInput :model-value="searchKeyword" placeholder="Search for groups" class="w-full" />
         <div class="mt-3 flex-1 overflow-auto">
-            <div v-if="false" class="text-gray-400 text-center p-3">
+            <div v-if="true" class="text-gray-400 text-center p-3">
                 You are not joined to any groups
             </div>
             <div v-else>
